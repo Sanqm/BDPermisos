e2fec43856e57e768f63171e32e2bd640e2f393d
Sanqm <sandra.queimadelos@gmail.com>
2024-01-25 20:16:40 +0100

    Ejercicio Permisos

---
+++ app/Controllers/UsuarioSistemaController.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Com\Daw2\Controllers;
+
+class UsuarioSistemaController extends \Com\Daw2\Core\BaseController {
+       
+  
+    function mostrarTodos(){
+        $data = [];
+        $data['titulo'] = 'Todos los usuarios';
+        $data['seccion'] = '/usuarios-sistema';
+        
+        $modelo = new \Com\Daw2\Models\UsuarioSistemaModel();
+        $data['usuarios'] = $modelo->getAll();                
+        
+        $this->view->showViews(array('templates/header.view.php', 'usuario_sistema.view.php', 'templates/footer.view.php'), $data);
+    }    
+    
+    function editar() {
+      
+        $data['titulo'] = 'Todos los usuarios';
+        $data['seccion'] = '/usuarios-sistema';
+
+       
+        $modelo = new \Com\Daw2\Models\UsuarioSistemaModel();
+        $data['usuarios'] = $modelo->getAll();
+        $auxusuariomodel = new \Com\Daw2\Models\AuxUsuarioModel();
+        $idiomas = $auxusuariomodel->getAllLanguages();
+        $roles= $auxusuariomodel->getAllRols();
+        
+        
+         $input = filter_var_array($_POST); // recogiendo el valor del todos los input de la vista  ?? es mas correcto que ponerlo en un input entero.
+        
+        ///pasando datos del model a la vista psando por el controller 
+        $data = array(
+            'idiomas' => $idiomas,
+            'roles' =>$roles,
+            'email' =>$modelo->getEmailFiltter($_POST),
+            'input' =>$input
+            
+        );
+
+        $this->view->showViews(array('templates/header.view.php', 'formularioAlta.view.php', 'templates/footer.view.php'), $data);
+        /// usuario_sistema.view.php
+    }
+}
